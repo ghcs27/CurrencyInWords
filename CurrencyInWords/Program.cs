d7a0af97d6a99a2a5d8e6e120aa2b8dc86f5a233
Christian <junkjunk@outlook.de>
2023-04-15 17:44:40 +0200

    Minimal console program

--- CurrencyInWords/Program.cs
+++ CurrencyInWords/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using CurrencyInWords;
+
+// Read number from console and write converted output back. No error handling.
+string input = Console.ReadLine() ?? "";
+string words = DollarAmount.Parse(input).ToWords();
+Console.WriteLine(words);
