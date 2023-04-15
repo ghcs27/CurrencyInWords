ba586572f9781a2a8ada344a3462341e4ea60f01
Christian <junkjunk@outlook.de>
2023-04-15 19:32:27 +0200

    Error handling for CLI

--- CurrencyInWords/Program.cs
+++ CurrencyInWords/Program.cs
@@ -1,6 +1,16 @@
 ﻿using CurrencyInWords;
 
-// Read number from console and write converted output back. No error handling.
+// Read number from console and write converted output back.
 string input = Console.ReadLine() ?? "";
-string words = DollarAmount.Parse(input).ToWords();
-Console.WriteLine(words);
+string words;
+try {
+    words = DollarAmount.Parse(input).ToWords();
+    Console.WriteLine(words);
+} catch (ArgumentException ex) {
+    // Value too large or not whole cents
+    Console.WriteLine(ex.Message);
+} catch (FormatException) {
+    Console.WriteLine("Input could not be recognized as a number. Please use ',' (comma) as decimal separator and spaces as optional thousands separator");
+} catch (OverflowException) {
+    Console.WriteLine("Value is too large or too small to be internally representable");
+}
