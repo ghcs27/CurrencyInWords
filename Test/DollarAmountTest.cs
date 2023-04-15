b1951ab05cadbb0afcab833f11162341c07c270b
Christian <junkjunk@outlook.de>
2023-04-15 18:21:35 +0200

    Add unit tests for all specified cases

--- Test/DollarAmountTest.cs
+++ Test/DollarAmountTest.cs
@@ -6,11 +6,16 @@ namespace Test;
 public class DollarAmountTest
 {
     [TestMethod]
-    public void TestZeroDollars() {
-        string input = "0";
+    [DataRow("0", 0, "zero dollars")]
+    [DataRow("1", 100, "one dollar")]
+    [DataRow("25,1", 2510, "twenty-five dollars and ten cents")]
+    [DataRow("0,01", 1, "zero dollars and one cent")]
+    [DataRow("45 100", 4510000, "forty-five thousand one hundred dollars")]
+    [DataRow("999 999 999,99", 999_999_999_99, "nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine dollars and ninety-nine cents")]
+    public void TestDollarsToWords(string input, long cents, string expectedWords) {
         DollarAmount amount = DollarAmount.Parse(input);
-        Assert.AreEqual(0m, amount.Dollars);
+        Assert.AreEqual(cents, amount.Cents);
         string words = amount.ToWords();
-        Assert.AreEqual("zero dollars", words);
+        Assert.AreEqual(expectedWords, words);
     }
 }
