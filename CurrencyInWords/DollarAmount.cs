c9a012938f3160842175d6461ff362c3d1d06e68
Christian <junkjunk@outlook.de>
2023-04-15 16:44:13 +0200

    Add helper function to convert numbers to words

--- CurrencyInWords/DollarAmount.cs
+++ CurrencyInWords/DollarAmount.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel;
+using System.Globalization;
 
 namespace CurrencyInWords;
 
@@ -65,5 +66,46 @@ public readonly struct DollarAmount
             }
         );
         return new DollarAmount { Dollars = dollars };
+    }
+
+    // Helper: Convert a positive number up to 999 999 999 to english words
+    private static string NumberToWords(long num) {
+        if (num <= 9) {
+            string[] digitWords = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            return digitWords[num];
+        } else if (num <= 19) {
+            string[] teenWords = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            return teenWords[num - 10];
+        } else if (num <= 99) {
+            string[] tensWords = { "", "", "twenty", "thirty", "fourty", "sixty", "seventy", "eighty", "ninety" };
+            string tens = tensWords[num / 10];
+            if (num % 10 == 0) {
+                return tens;
+            } else {
+                // Recurse to use previous cases for one's digit
+                return $"{tens}-{NumberToWords(num % 10)}";
+            }
+        } else if (num < 999) {
+            // Recurse to use previous cases for hundred's digit and rest
+            if (num % 100 == 0) {
+                return $"{NumberToWords(num / 100)} hundred";
+            } else {
+                return $"{NumberToWords(num / 100)} hundred {NumberToWords(num % 100)}";
+            }
+        } else if (num < 999_999) {
+            if (num % 1000 == 0) {
+                return $"{NumberToWords(num / 1000)} thousand";
+            } else {
+                return $"{NumberToWords(num / 1000)} thousand {NumberToWords(num % 1000)}";
+            }
+        } else if (num < 999_999_999) {
+            if (num % 1000 == 0) {
+                return $"{NumberToWords(num / 1000_000)} million";
+            } else {
+                return $"{NumberToWords(num / 1000_000)} million {NumberToWords(num % 1000_000)}";
+            }
+        } else {
+            throw new ArgumentOutOfRangeException("num is larger than 999 999 999; not supported");
+        }
     }
 }
